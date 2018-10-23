27668a7e67f1b594b189ea3099f93ad613d5fcd5
piyush ghate <piyushghate17@gmail.com>
2018-10-24 01:12:45 +0530

    Finally got the Adset working and commiting the files without few keys!

---
+++ playground/adset.py
@@ -0,0 +1,51 @@
+from facebook_business.adobjects.adset import AdSet
+# from facebook_business.adobjects.targeting import Targeting
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.api import FacebookAdsApi
+# adset.update({
+#     AdSet.Field.name: 'My Ad Set',
+#     AdSet.Field.campaign_id: 23843112406190495,
+#     AdSet.Field.daily_budget: 1000,
+#     AdSet.Field.billing_event: AdSet.BillingEvent.impressions,
+#     AdSet.Field.optimization_goal: AdSet.OptimizationGoal.reach,
+#     AdSet.Field.bid_amount: 2,
+#     AdSet.Field.targeting: {
+#         Targeting.Field.geo_locations: {
+#             'countries': ['IN'],
+#         },
+#     },
+# })
+
+# adset.remote_create(params={
+#     'status': AdSet.Status.paused,
+# })
+# print(adset)
+
+# geo_locations: {
+#             'countries': ['IN'],
+#         }
+
+access_token = ''
+id = 'act_<AccID>'
+
+FacebookAdsApi.init(access_token=access_token)
+
+fields = []
+params = {
+  'name': 'First Ad Set',
+  'campaign_id': '23843112406190495',
+  'campaign': 'My campaign',
+  'billing_event': 'LINK_CLICKS',
+  'optimization_goal': 'LINK_CLICKS',
+  'daily_budget': '20000',
+  'bid_amount': '2',
+  'targeting' : {'geo_locations':{'countries':['IN']}},
+  'status': 'PAUSED',
+}
+
+adset = AdAccount(id).create_ad_set(
+    fields = fields,
+    params = params,
+)
+
+print(adset)
--- playground/test.py
+++ playground/test.py
@@ -2,10 +2,10 @@ from facebook_business.adobjects.adaccount import AdAccount
 from facebook_business.adobjects.campaign import Campaign
 from facebook_business.api import FacebookAdsApi
 
-access_token = 'ACCESS TOKEN'
-app_secret = 'SECRET KEY'
+access_token = ''
+app_secret = ''
 app_id = '2215442238727389'
-id = 'act_Account ID'
+id = 'act_257672218227370'
 FacebookAdsApi.init(access_token=access_token)
 
 fields = [
@@ -15,8 +15,11 @@ params = {
   'objective': 'LINK_CLICKS',
   'status': 'PAUSED',
 }
-print(AdAccount(id).create_campaign(
+
+campaign = AdAccount(id).create_campaign(
   fields=fields,
   params=params,
-))
+)
+
+print(campaign)
 
